de894da5b35d3eb60bb5153285809e53500cb9f8
Mark Kovalev <restonich@gmail.com>
2017-12-15 00:36:27 +0300

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Async_Parser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("URL: ");
+      string url = Console.ReadLine();
+
+      Parser parser = new Parser();
+
+      var all_pages = parser.Parse(new[] { new Page(url) }).Result;
+
+      foreach (var page in all_pages)
+        Console.WriteLine("{0} ({1})", page.url, page.html_code.Length);
+
+      Console.WriteLine("If you're reading this, you have dozen of patiences.");
+      Console.ReadKey();
+    }
+  }   
+}
